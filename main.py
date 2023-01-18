e8cb3ea8b8073572cab96f9b0a5576fe2e441aa6
Margaux Masson-Forsythe <margaux.masson21@gmail.com>
2023-01-18 11:58:36 -0500

    add file upload to upload pretrained weights

--- main.py
+++ main.py
@@ -93,52 +93,54 @@ images_input = glob.glob("images/content-images/*.tif")
 images_input.sort()
 images_input = [path.split('/')[-1] for path in images_input]
 
-models = glob.glob("models/**/*.pt")
-model = st.sidebar.selectbox(
-    'Select Model',
-    (models)
-)
+# models = glob.glob("models/**/*.pt")
+# model = st.sidebar.selectbox(
+#     'Select Model',
+#     (models)
+# )
+model = st.sidebar.file_uploader("Upload Model here", type=["pt"])
 
-image_file = st.sidebar.file_uploader("Upload Image", type=["jpg", "png", "tif"])
-if image_file is not None:
-    image_name = image_file.name
-    print(image_name)
-    if '.tif' in image_name:
-        print('tifffile -- not need for mask image')
-        stack = tifffile.imread(image_file)
-        if stack.shape[0:2] != (1024, 1024):
-            st.write('Wrong image size -- cropping image to 1024x1024')
-            print("Wrong image size -- cropping image")
-            # TODO  center crop?
-            # stack = cv2.resize(stack, (1024, 1024), cv2.INTER_NEAREST)
-            stack = stack[0:1024, 0:1024, :]
-        post_uploads(stack, image_name)
-    else:
-        mask_file = st.sidebar.file_uploader("Upload Image with reforestation label", type=["jpg", "png"])
-        image = np.array(Image.open(image_file))
-        if mask_file is not None:
-            mask = np.array(Image.open(mask_file))
-            print(f"Mask: {mask.shape}")
-            if mask.shape[0:2] != (1024, 1024):
+if model is not None:
+    image_file = st.sidebar.file_uploader("Upload Image", type=["jpg", "png", "tif"])
+    if image_file is not None:
+        image_name = image_file.name
+        print(image_name)
+        if '.tif' in image_name:
+            print('tifffile -- not need for mask image')
+            stack = tifffile.imread(image_file)
+            if stack.shape[0:2] != (1024, 1024):
                 st.write('Wrong image size -- cropping image to 1024x1024')
-                if len(mask.shape) != 3:
-                    mask = np.expand_dims(mask, axis=-1)
-                mask = mask[0:1024, 0:1024, :]
-                image = image[0:1024, 0:1024, :]
-            mask_pixels = np.all(mask >= [255, 255, 255], axis=-1)
-            non_mask_pixels = np.any(mask < [255, 255, 255], axis=-1)
-
-            mask[mask_pixels] = [255, 255, 255]
-            mask[non_mask_pixels] = [0, 0, 0]
-            mask = multi_dil(mask, 5)
-            mask = multi_ero(mask, 5)
-            mask = mask[:, :, 0]
-            assert np.unique(mask)[0] == 0
-
-            stack = np.dstack((image, mask))
-            image_name = image_name.replace('jpg', 'tif')
-            print("Mask generated")
+                print("Wrong image size -- cropping image")
+                # TODO  center crop?
+                # stack = cv2.resize(stack, (1024, 1024), cv2.INTER_NEAREST)
+                stack = stack[0:1024, 0:1024, :]
             post_uploads(stack, image_name)
+        else:
+            mask_file = st.sidebar.file_uploader("Upload Image with reforestation label", type=["jpg", "png"])
+            image = np.array(Image.open(image_file))
+            if mask_file is not None:
+                mask = np.array(Image.open(mask_file))
+                print(f"Mask: {mask.shape}")
+                if mask.shape[0:2] != (1024, 1024):
+                    st.write('Wrong image size -- cropping image to 1024x1024')
+                    if len(mask.shape) != 3:
+                        mask = np.expand_dims(mask, axis=-1)
+                    mask = mask[0:1024, 0:1024, :]
+                    image = image[0:1024, 0:1024, :]
+                mask_pixels = np.all(mask >= [255, 255, 255], axis=-1)
+                non_mask_pixels = np.any(mask < [255, 255, 255], axis=-1)
+
+                mask[mask_pixels] = [255, 255, 255]
+                mask[non_mask_pixels] = [0, 0, 0]
+                mask = multi_dil(mask, 5)
+                mask = multi_ero(mask, 5)
+                mask = mask[:, :, 0]
+                assert np.unique(mask)[0] == 0
+
+                stack = np.dstack((image, mask))
+                image_name = image_name.replace('jpg', 'tif')
+                print("Mask generated")
+                post_uploads(stack, image_name)
 
 
 
